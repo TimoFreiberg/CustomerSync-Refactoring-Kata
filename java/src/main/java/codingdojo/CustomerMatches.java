192a948f4289882464626601da0bfd0cf2c5431a
Timo Freiberg <timo.freiberg@gmail.com>
2020-02-08 14:45:51 +0100

    remove obsolete matchTerm in CustomerMatches

--- java/src/main/java/codingdojo/CustomerMatches.java
+++ java/src/main/java/codingdojo/CustomerMatches.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 
 public class CustomerMatches {
     private Collection<Customer> duplicates = new ArrayList<>();
-    private String matchTerm;
     private Customer customer;
 
     public Customer getCustomer() {
@@ -20,14 +19,6 @@ public class CustomerMatches {
 
     public Collection<Customer> getDuplicates() {
         return duplicates;
-    }
-
-    public String getMatchTerm() {
-        return matchTerm;
-    }
-
-    public void setMatchTerm(String matchTerm) {
-        this.matchTerm = matchTerm;
     }
 
     public void setCustomer(Customer customer) {
