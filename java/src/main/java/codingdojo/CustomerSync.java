2cc8e6c07ae888b6aadc590514a5dbe8eb18a025
Timo Freiberg <timo.freiberg@gmail.com>
2020-02-08 11:00:09 +0100

    compiler warning: make updateCustomer return void

--- java/src/main/java/codingdojo/CustomerSync.java
+++ java/src/main/java/codingdojo/CustomerSync.java
@@ -52,8 +52,8 @@ public class CustomerSync {
         }
     }
 
-    private Customer updateCustomer(Customer customer) {
-        return this.customerDataAccess.updateCustomerRecord(customer);
+    private void updateCustomer(Customer customer) {
+        this.customerDataAccess.updateCustomerRecord(customer);
     }
 
     private void updateDuplicate(ExternalCustomer externalCustomer, Customer duplicate) {
